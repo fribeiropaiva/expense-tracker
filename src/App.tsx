26d87991ac3063e25b271a34ca53922f540d9ceb
ribeiro240 <filipepaiva240@gmail.com>
2021-04-30 17:28:29 -0300

    Add data caching

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,37 @@ import ExpensesContainer from './components/ExpensesContainer/ExpensesContainer'
 import { Expense } from './components/ExpensesContainer/ExpensesContainer';
 
 function App() {
-  const [expensesList, setExpensesList] = useState<Expense[]>([]);
+  const [expensesList, setExpensesList] = useState<Expense[]>(() => {
+    let cachedData = null;
+
+    try {
+      cachedData = localStorage.getItem('expense-tracker');
+    } catch(e) {
+      console.error('Error in getting data from cache.', e);
+    }
+
+    return cachedData ? JSON.parse(cachedData) : [];
+  });
+
+  function updateLocalStorage(updatedData: Expense[]) {
+    const cacheKey = 'expense-tracker';
+
+    try {
+      localStorage.setItem(cacheKey, JSON.stringify(updatedData));
+    } catch(e) {
+      console.error('Error in caching data.', e);
+    }
+  }
 
   function updateExpensesList(newExpense: Expense) {
     setExpensesList((expensesList: Expense[]) => [...expensesList, newExpense]);
+    updateLocalStorage([...expensesList, newExpense]);
   }
 
   function handleRemove(id: string) {
     const updatedExpensesList = expensesList.filter(expense => expense.id !== id);
     setExpensesList(updatedExpensesList);
+    updateLocalStorage(updatedExpensesList);
   }
 
   return (
